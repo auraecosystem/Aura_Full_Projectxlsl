c96f6c80008051791fd1de37a5b9282609441350
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-17 19:55:41 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -79,3 +79,5 @@ npm install --save-dev husky lint-staged
 npx husky init
 node --eval "fs.writeFileSync ('.husky/pre-commit','npx lint-staged\n')"
 npx prettier . --write
+
+npm install --save-dev lint-staged
