3a3fccc59d5cde8bd06baa6debc860242df66804
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-18 15:03:15 +0100

    Create build.ts

---
+++ aura.dev/web-features/scripts/build.ts
@@ -0,0 +1,146 @@
+import { DefinedError } from "ajv";
+import { getStatus } from "compute-baseline";
+import stringify from "fast-json-stable-stringify";
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+import { basename } from "node:path";
+import winston from "winston";
+import yargs from "yargs";
+import * as data from "../index.js";
+import { isOrdinaryFeatureData } from "../type-guards.js";
+import { validate } from "./validate.js";
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple(),
+  ),
+  transports: new winston.transports.Console(),
+});
+
+const rootDir = new URL("..", import.meta.url);
+
+yargs(process.argv.slice(2))
+  .scriptName("build")
+  .command({
+    command: "package",
+    describe: "Generate the web-features npm package",
+    handler: buildPackage,
+  })
+  .command({
+    command: "extended-json",
+    describe: "Generate a web-features JSON file with BCD per-key statuses",
+    handler: buildExtendedJSON,
+  })
+  .parseSync();
+
+function buildPackage() {
+  const packageDir = new URL("./packages/web-features/", rootDir);
+  const filesToCopy = [
+    "LICENSE.txt",
+    "types.quicktype.ts",
+    "types.ts",
+    "schemas/data.schema.json",
+  ];
+
+  if (!valid(data)) {
+    logger.error("Data failed schema validation. No package built.");
+    process.exit(1);
+  }
+
+  const json = stringify(data);
+  const path = new URL("data.json", packageDir);
+  fs.writeFileSync(path, json);
+  for (const file of filesToCopy) {
+    fs.copyFileSync(
+      new URL(file, rootDir),
+      new URL(basename(file), packageDir),
+    );
+  }
+  execSync("npm install", {
+    cwd: "./packages/web-features",
+    encoding: "utf-8",
+  });
+  execSync("npm run prepare", {
+    cwd: "./packages/web-features",
+    encoding: "utf-8",
+  });
+}
+
+function buildExtendedJSON() {
+  for (const [id, featureData] of Object.entries(data.features)) {
+    if (!isOrdinaryFeatureData(featureData)) {
+      continue;
+    }
+
+    if (
+      Array.isArray(featureData.compat_features) &&
+      featureData.compat_features.length &&
+      featureData.status
+    ) {
+      featureData.status.by_compat_key = {};
+      for (const key of featureData.compat_features) {
+        featureData.status.by_compat_key[key] = getStatus(id, key);
+      }
+    }
+  }
+
+  if (!valid(data)) {
+    logger.error("Data failed schema validation. No JSON file written.");
+    process.exit(1);
+  }
+
+  fs.writeFileSync(new URL("./data.extended.json", rootDir), stringify(data));
+}
+
+function valid(data: any): boolean {
+  const valid = validate(data);
+  if (!valid) {
+    // TODO: turn on strictNullChecks, fix all the errors, and replace this with:
+    // const errors = validate.errors;
+    const errors = validate.errors as DefinedError[];
+    for (const error of errors) {
+      logger.error(`${error.instancePath}: ${error.message}`);
+    }
+    return false;
+  }
+  return true;
+}
+
+import { getStatus } from 'compute-baseline';
+
+function getBaselineStatus (featureId, bcdKey) {
+  return getStatus(featureId, bcdKey);
+}
+
+"subgrid": {
+  "caniuse": "css-subgrid",
+  "compat_features": [
+    "css.properties.grid-template-columns.subgrid",
+    "css.properties.grid-template-rows.subgrid"
+  ],
+  "description": "The subgrid value for the grid-template-columns and grid-template-rows properties allows a grid item to inherit the grid definition of its parent grid container.",
+  "description_html": "The <code>subgrid</code> value for the <code>grid-template-columns</code> and <code>grid-template-rows</code> properties allows a grid item to inherit the grid definition of its parent grid container.",
+  "group": "grid",
+  "name": "Subgrid",
+  "spec": "https://drafts.csswg.org/css-grid-2/#subgrids",
+  "status": {
+    "baseline": "low",
+    "baseline_low_date": "2023-09-15",
+    "support": {
+      "chrome": "117",
+      "chrome_android": "117",
+      "edge": "117",
+      "firefox": "71",
+      "firefox_android": "79",
+      "safari": "16",
+      "safari_ios": "16"
+    }
+  }
+}
+
+import { features } from 'web-features';
+
+function getBaselineStatus (featureId) {
+  return features[featureId]?.status.baseline;
+}
