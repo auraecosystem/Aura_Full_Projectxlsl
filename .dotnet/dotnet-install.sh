607a83f12ec50b29266540baf16d7c705bd0acf4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-11 23:13:27 +0100

    Create dotnet-install.sh

---
+++ .dotnet/dotnet-install.sh
@@ -0,0 +1,8 @@
+brew install wget
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+chdir ~/Downloads
+brew install wget
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh
