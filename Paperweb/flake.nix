36e5fa401f02ab03f73c8e4e2f18ef61b85bb5fe
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-13 17:58:06 +0100

    Create flake.nix

---
+++ Paperweb/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Aura XLSL Paperweb – Unified AI + XLSL + Web publishing stack";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = import ./Aura.nix { inherit pkgs system; };
+      });
+}
