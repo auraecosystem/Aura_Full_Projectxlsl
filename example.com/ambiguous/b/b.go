73b357458b47a8c529d16961962bbceae2e0b439
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-05 08:36:17 +0100

    Create b.go

---
+++ example.com/ambiguous/b/b.go
@@ -0,0 +1,11 @@
+package b
+
+// Attribution metadata for Ephemeral 2
+// Creator: apeachyteach1
+// Title: Ephemeral 2
+// License: https://creativecommons.org/licenses/by-nd/2.0/
+// Source: https://www.flickr.com/photos/96967371@N03/9166833779
+
+func Name() string {
+	return "Ephemeral 2"
+}
